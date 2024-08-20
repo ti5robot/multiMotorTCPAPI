7255f28a04603c13b0cbe8f972f8f233d42d2c67
Mrhouse <Mrhouse.ma@outlook.com>
2024-08-20 17:09:34 +0800

    update

--- src/gcc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CPLUS_INCLUDE_PATH=/mnt/hgfs/multi_motor_new/include:$CPLUS_INCLUDE_PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/hgfs/multi_motor_new/include/can
-g++ main.cpp  Ti5BASIC.cpp Ti5LOGIC.cpp  mathfunc.cpp  tool.cpp socket_service.cpp -L../include/can -lmylibscan -lcontrolcan -o multi_motor_new
